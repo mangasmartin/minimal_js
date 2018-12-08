daa5feb005ae9cdcd4bc05061fc9deea85a5aef6
L. Enrique Mangas Martín <mangasmartin@yahoo.es>
2018-12-08 22:27:54 +0100

    Update css.js

--- css.js
+++ css.js
@@ -1,3 +1,6 @@
+//Prototype style settings as a new method called css
+//It must be set before a calling
+
 Object.prototype.css = function(param){
   for (var p in param){
     this.style[p] = param[p];
