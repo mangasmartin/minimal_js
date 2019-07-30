14d8c4e162235b9cbc2933aad64f3d321410c423
L. Enrique Mangas Martín <mangasmartin@yahoo.es>
2019-07-30 14:08:25 +0200

    Update fullscreen.js
    Add the comment that click event is mandatory to work everywhere.

--- fullscreen.js
+++ fullscreen.js
@@ -1,5 +1,17 @@
-function fullscreen(orient){
-  var elm = document.body;
+/*
+How to call it if a button with id 'full' and a wrapper with id 'display' are present in DOM:
+
+document.getElementById("full").addEventListener("click", function(){
+  var elm = document.getElementById("display");
+  fullscreen(elm,"landscape");
+});
+
+Important: event must be "click" and not a touch event in order to work on Android default browser
+Orientation can take only two values: "portrait" and "landscape"
+*/
+
+function fullscreen(elm,orient){
+  elm = elm || document.body;
   if (elm.mozRequestFullScreen) {
     elm.mozRequestFullScreen();
   }
@@ -12,6 +24,6 @@ function fullscreen(orient){
   if (orient && screen.orientation.lock){
     screen.orientation.lock(orient);
   } else {
-    //TODO show an advicement asking to the user to change manually the orientation device
+    //Show an advicement asking to the user to change manually the orientation device
   }
 }
