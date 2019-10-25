da1e4a5fbc4d885fa17455eef11b205c7960bdaa
L. Enrique Mangas Martín <mangasmartin@yahoo.es>
2019-10-25 14:50:14 +0200

    Update random_num.js
    Added minimum number.

--- random_num.js
+++ random_num.js
@@ -1,12 +1,14 @@
 function random_num(max, last){
+  //min: min number
   //max: max number
   //last: last number used. If set avoid repetition
 
-  var n = Math.round(Math.random() * (max - 1));
+  if (!min) min = 0;
+  var n = Math.round(Math.random() * (max - 1)) + min;
   if (last){
     if (n == last){
       n ++;
-      if (n == max) n = 0;
+      if (n == max) n = min;
     }
   }
   return n;
