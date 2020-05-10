8e7ae72bf018b204dcc3f5646213222804b9eedc
L. Enrique Mangas Martín <mangasmartin@yahoo.es>
2020-05-10 23:08:34 +0200

    Update random_num.js
    Fix last bug

--- random_num.js
+++ random_num.js
@@ -2,13 +2,13 @@ function random_num(min, max, last){
   //min: min number (Optional)
   //max: max number (Mandatory)
   //last: last number used: if it's set it avoids repetition only for next number (Optional)
-
- if (!min) min = 0;
+  if (!min) min = 0;
+  if (min == max) return min;
   var n = Math.round(Math.random() * (max - 1)) + min;
   if (last){
     if (n == last){
       n ++;
-      if (n => max + min) n = min;
+      if (n > max) n = min;
     }
   }
   return n;
